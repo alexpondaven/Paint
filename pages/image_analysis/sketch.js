ecb118d710cdec7ddc726d72d244008dc6e2b933
alexpondaven <alex.pondaven@gmail.com>
2020-11-16 00:46:51 +0000

    merge sort project

--- pages/image_analysis/sketch.js
+++ pages/image_analysis/sketch.js
@@ -2,12 +2,12 @@ let img; // Declare variable 'img'
 
 function setup() {
 	createCanvas(720, 400);
-	img = loadImage('images/lena.jpg');
+	img = loadImage('https://raw.githubusercontent.com/alexpondaven/jsprojects/master/pages/image_analysis/images/lena.jpg');
 }
 
 function draw() {
 	// Display image at actual size at (0,0)
 	image(img,0,0)
 	// Display image at (0, height/2) at half size
-	image(img,0,height/2,img.width/2,img.height/2);
+	image(img,0,height/2,img.width,img.height/2);
 }
---
+++ pages/merge_sort/sketch.js
@@ -0,0 +1,18 @@
+let arr = [10,4,5,9,15,20,3,4]
+
+function setup() {
+	createCanvas(
+		window.innerWidth,
+		window.innerHeight
+	);
+}
+
+function draw() {
+	background(0);
+	// Plot array as lines
+
+}
+
+// function merge_sort() {
+
+// }
