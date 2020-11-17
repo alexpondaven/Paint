76658b16d80608632ac02220d38f1184fc898355
alexpondaven <alex.pondaven@gmail.com>
2020-11-17 22:54:49 +0000

    visualise bars

--- pages/merge_sort/sketch.js
+++ pages/merge_sort/sketch.js
@@ -1,4 +1,4 @@
-let arr = [10,4,5,9,15,20,3,4]
+let array = [10,40,50,20,80];
 
 function setup() {
 	createCanvas(
@@ -8,11 +8,25 @@ function setup() {
 }
 
 function draw() {
+	let width = window.innerWidth;
+	let height = window.innerHeight;
 	background(0);
 	// Plot array as lines
+	 // array to sort
+	let n = array.length;
+	for (let i=0;i<n;i++){
+		rect(i*width/n,0,width/n,array[i]);
+		fill(255);
+		text(array[i], (i+0.5)*width/n,array[i]+10)
+	}
+	
 
 }
 
-// function merge_sort() {
-
+// function merge_sort(X) {
+// 	if (X.length<=1){
+// 		return X;
+// 	}
+// 	let mid = Math.floor(X.length/2);
+// 	let L = mergesort(X.slice())
 // }
