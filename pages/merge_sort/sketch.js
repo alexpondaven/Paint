b55d6b5950fed0207db641b21c94af81314be986
alexpondaven <alex.pondaven@gmail.com>
2020-11-18 00:08:27 +0000

    merge sort works

--- pages/merge_sort/sketch.js
+++ pages/merge_sort/sketch.js
@@ -1,32 +1,64 @@
 let array = [10,40,50,20,80];
+let all_array = [];
+let s_array = mergesort(array);
+
+console.log(array);
+console.log(s_array);
 
 function setup() {
 	createCanvas(
 		window.innerWidth,
 		window.innerHeight
 	);
-}
-
-function draw() {
-	let width = window.innerWidth;
-	let height = window.innerHeight;
+	// Draw original array
 	background(0);
-	// Plot array as lines
-	 // array to sort
 	let n = array.length;
 	for (let i=0;i<n;i++){
 		rect(i*width/n,0,width/n,array[i]);
 		fill(255);
 		text(array[i], (i+0.5)*width/n,array[i]+10)
 	}
+}
+
+function draw() {
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+	// background(0);
+	// Plot array as lines
+	 // array to sort
+	
+	
 	
 
 }
 
-// function merge_sort(X) {
-// 	if (X.length<=1){
-// 		return X;
-// 	}
-// 	let mid = Math.floor(X.length/2);
-// 	let L = mergesort(X.slice())
-// }
+function mergesort(X) {
+	if (X.length<=1){
+		return X;
+	}
+	let mid = Math.floor(X.length/2);
+	let L = mergesort(X.slice(0,mid));
+	let R = mergesort(X.slice(mid,X.length));
+	let out = [];
+	let i=0;
+	let j=0;
+	while (i<L.length || j<R.length){
+		if (i==L.length){ // if L finished, append rest of R
+			out.push(R[j]);
+			j++;
+		} else if (j==R.length){ // if R finished, append rest of L
+			out.push(L[i]);
+			i++;
+		} else { // compare values of L and R and append lowest one
+			if (L[i] < R[j]){
+				out.push(L[i]);
+				i++;
+			} else {
+				out.push(R[j]);
+				j++;
+			}
+		}
+	}
+	console.log(out);
+	return out;
+}
