0e3b65942db360f1f36031cb669f8d4a7e90e068
alexpondaven <alex.pondaven@gmail.com>
2020-11-20 00:47:29 +0000

    improved particle collisions and added colour

--- pages/class/particle.js
+++ pages/class/particle.js
@@ -1,34 +1,53 @@
 function Particle(x, y) {
+	/**
+	get better collision function
+	add collision animation
+	**/
 	this.x = x;
 	this.y = y;
+	this.r = 50;
 
 	this.xspeed = 10;
 	this.yspeed = 10;
 
+	this.colour = color(255, 0, 255);
+
 	this.update = function() {
 		this.x += this.xspeed;
 		this.y += this.yspeed;
-		if (this.x > width){
-			this.x = width;
+		if (this.x > width-this.r/2){
+			this.x = width-this.r/2;
 			this.xspeed = -this.xspeed;
+			this.colour = color(Math.floor(Math.random() * 255),
+							Math.floor(Math.random() * 255),
+							Math.floor(Math.random() * 255));
 		}
-		if (this.x < 0){
-			this.x = 0;
+		if (this.x < this.r/2){
+			this.x = this.r/2;
 			this.xspeed = -this.xspeed;
+			this.colour = color(Math.floor(Math.random() * 255),
+							Math.floor(Math.random() * 255),
+							Math.floor(Math.random() * 255));
 		}
-		if (this.y > height){
-			this.y = height;
+		if (this.y > height - this.r/2){
+			this.y = height - this.r/2;
 			this.yspeed = -this.yspeed;
+			this.colour = color(Math.floor(Math.random() * 255),
+							Math.floor(Math.random() * 255),
+							Math.floor(Math.random() * 255));
 		}
-		if (this.y < 0){
-			this.y = 0;
+		if (this.y < this.r/2){
+			this.y = this.r/2;
 			this.yspeed = -this.yspeed;
+			this.colour = color(Math.floor(Math.random() * 255),
+							Math.floor(Math.random() * 255),
+							Math.floor(Math.random() * 255));
 		}
 
 	}
 
 	this.show = function() {
-		fill(255);
-		circle(this.x,this.y,10);
+		fill(this.colour);
+		circle(this.x,this.y,this.r);
 	}
 }
